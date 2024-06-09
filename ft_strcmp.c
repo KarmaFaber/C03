c528d8abb86ece38f057d9f5d8a69fb4f7eea67d
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-06-09 21:17:06 +0200

    ex00

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -1,3 +1,50 @@
+int ft_strcmp(char *s1, char *s2) 
+{
+    int i;
+    
+    i = 0;
+    while (s1[i] != '\0' && s2[i] != '\0') 
+    {
+        if (s1[i] != s2[i]) 
+        {
+            return s1[i] - s2[i];
+        }
+        i++;
+    }
+    return s1[i] - s2[i];
+}
+
+#include <stdio.h>
+int main() 
+{
+    char *cadena1 = "Hola mundo";
+    char *cadena2 = "Hola mundo";
+
+    int resultado=ft_strcmp(cadena1, cadena2);
+    printf("%i", resultado);
+
+    return 0;
+}
+
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*tambien vale este formato
+#include <unistd.h>
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2 && *s1)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+*/
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*tambien vale este formato:
 int		ft_strcmp(char *s1, char *s2)
 {
 	int i;
@@ -36,19 +83,19 @@ int main() {
     return 0;
 }
 
+*/
+/*--------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*EXPLICACION DE INT DEVUELTO: */
+/*
+char *s1 = "hola";
+char *s2 = "hola";
+int resultado = ft_strcmp(s1, s2);  // resultado será 0
 
+char *s1 = "hola";
+char *s2 = "holA";
+int resultado = ft_strcmp(s1, s2);  // resultado será 'a' - 'A' = 97 - 65 = 32
 
-/*---------------------------------------*/
-/*tambien vale este formato
-#include <unistd.h>
-
-int		ft_strcmp(char *s1, char *s2)
-{
-	while (*s1 == *s2 && *s1)
-	{
-		s1++;
-		s2++;
-	}
-	return (*s1 - *s2);
-}
+char *s1 = "hola";
+char *s2 = "holanda";
+int resultado = ft_strcmp(s1, s2);  // resultado será '\0' - 'n' = 0 - 110 = -110
 */
