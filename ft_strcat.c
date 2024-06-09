9c3f6acd3e2e7f9b40b571ec8006dda967b2cc31
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-06-09 21:57:54 +0200

    ex03

---
+++ ft_strcat.c
@@ -0,0 +1,43 @@
+int ft_strlen(char *str)
+{
+    int count;
+    count = 0;
+    
+    while (str[count] != '\0')
+    {
+        count ++ ;
+    }
+    return (count);
+}
+
+char *ft_strcat(char *dest, char *src)
+{
+   int i;
+   int j; 
+   
+   i = 0;
+   j = ft_strlen(dest);
+   while (dest[i] != '\0')
+   {
+       dest[j] = src [i];
+       
+       i ++ ;
+       j ++;
+   }
+    dest[j] = '\0';
+    
+    return dest;
+}
+
+#include <stdio.h>
+int main (void)
+{
+    char dest[30] = "hola ";
+    char src [] = "caracola";
+    
+    ft_strcat(dest, src);
+ 
+    printf("%s\n", dest);
+
+    return 0;
+}
