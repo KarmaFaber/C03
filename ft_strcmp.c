56f061619140f2fb864844e5845129785c23ffe8
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-06-09 11:38:33 +0200

    MAL-CORREGIR

---
+++ ft_strcmp.c
@@ -0,0 +1,27 @@
+int ft_strcmp(char *s1, char *s2)
+
+{
+    int i;
+    
+    i = 0;
+    while (s1[i] != '\0' || s2[i] != '\0')
+    {
+        if (s1[i] == s2[i])
+            return 0;
+        else if (s1[i] != s2[i])
+            return i;
+        
+        i ++ ;
+    }
+    
+    
+}
+
+int main() {
+    const char *cadena1 = "Hola mundo";
+    const char *cadena2 = "Hola mundo";
+
+    ft_strcmp(*cadena1, *cadena2)
+
+    return 0;
+}
