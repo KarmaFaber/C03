3ab5b5b4ee59099ce72c9e7e52ca936ae6f3f2b0
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-06-09 21:06:53 +0200

    ex00

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -1,27 +1,54 @@
-int ft_strcmp(char *s1, char *s2)
-
+int		ft_strcmp(char *s1, char *s2)
 {
-    int i;
-    
-    i = 0;
-    while (s1[i] != '\0' || s2[i] != '\0')
-    {
-        if (s1[i] == s2[i])
-            return 0;
-        else if (s1[i] != s2[i])
-            return i;
-        
-        i ++ ;
-    }
-    
-    
+	int i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
 }
 
+#include <stdio.h>
 int main() {
-    const char *cadena1 = "Hola mundo";
-    const char *cadena2 = "Hola mundo";
+    char cadena1[] = "Hola mundo";
+    char cadena2[] = "Hola mUndo";
+    
+    int resultado = ft_strcmp(cadena1, cadena2);
 
-    ft_strcmp(*cadena1, *cadena2)
+    if (resultado == 0) {
+        printf("Las cadenas son iguales. Int = 0.");
+    } else {
+        printf("Las cadenas son diferentes.\n");
+
+        int i = 0;
+        while (cadena1[i] && cadena2[i] && cadena1[i] == cadena2[i]) {
+            i++;
+        }
+
+        if (cadena1[i] != cadena2[i]) {
+            printf("Diferencia en posición %d: '%c' (ASCII: %d) en cadena1 (s1), '%c' (ASCII: %d) en cadena2 (s2), Diferencia ASCII: %d\n", 
+                   i, cadena1[i], cadena1[i], cadena2[i], cadena2[i], cadena1[i] - cadena2[i]);
+        }
+    }
 
     return 0;
 }
+
+
+
+/*---------------------------------------*/
+/*tambien vale este formato
+#include <unistd.h>
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2 && *s1)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+*/
