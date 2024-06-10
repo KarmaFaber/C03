c16b71eb76ce2f5030501b1e9e5d580809ba3a6c
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-06-10 18:42:06 +0200

    ex04

--- ft_strstr.c
+++ ft_strstr.c
@@ -1,48 +1,44 @@
-char	*ft_strstr(char *str, char *to_find)
+char *ft_strstr(char *str, char *to_find)
 {
-	int i;
-	int temp;
-	char *original_str = str; // Guardamos el puntero original
+    int i;
 
-	if (*to_find == '\0')
-		return (str);
-		
-	while (*str != '\0')
-	{
-		if (*str == *to_find)
-		{
-			i = 0;
-			temp = 0;
-			while (to_find[i] != '\0')
-			{
-				if (str[i] != to_find[i])
-					temp = 1;
-				i++;
-			}
-			if (temp == 0)
-				return (str); 
-		}
-		str++;
-	}
-	return (NULL); 
+    if (*to_find == '\0')
+        return (str);
+
+    while (*str != '\0')
+    {
+        if (*str == *to_find)
+        {
+            i = 0;
+            while (to_find[i] != '\0')
+            {
+                if (str[i] != to_find[i])
+                    break;
+                i++;
+            }
+            if (to_find[i] == '\0')
+                return (str);
+        }
+        str++;
+    }
+    return ((char *)0); // Retorna un puntero nulo
 }
 
 #include <stdio.h>
-int		main(void)
+int main(void)
 {
-	char str[] = "Hola Caracola";
-	char to_find[] = "acol";
-	char *result;
+    char str[] = "Hola Caracola";
+    char to_find[] = "Ca";
+    char *result;
 
-	result = ft_strstr(str, to_find);
-	if (result != NULL)
-	{
-		printf("La subcadena se encuentra en: %s\n", result);
-		
-		printf("Posición: %ld\n", result - str);
-	}
-	else
-		printf("La subcadena no se encuentra en la cadena.\n");
+    result = ft_strstr(str, to_find);
+    if (result != (char *)0) // Compara con puntero nulo
+    {
+        printf("La subcadena se encuentra en: %s\n", result);
+        printf("Posición: %ld\n", result - str);
+    }
+    else
+        printf("La subcadena no se encuentra en la cadena.\n");
 
-	return 0;
+    return 0;
 }
