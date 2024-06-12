4d6f302e0fd5392460a641d6d777754c2925873f
Karma <52334627+KarmaFaber@users.noreply.github.com>
2024-06-12 11:54:52 +0200

    ex02_v1

--- ft_strcat.c
+++ ft_strcat.c
@@ -1,43 +1,29 @@
-int ft_strlen(char *str)
+char	*ft_strcat(char *dest, char *src)
 {
-    int count;
-    count = 0;
-    
-    while (str[count] != '\0')
-    {
-        count ++ ;
-    }
-    return (count);
-}
+	int i;
+	int j;
 
-char *ft_strcat(char *dest, char *src)
-{
-   int i;
-   int j; 
-   
-   i = 0;
-   j = ft_strlen(dest);
-   while (dest[i] != '\0')
-   {
-       dest[j] = src [i];
-       
-       i ++ ;
-       j ++;
-   }
-    dest[j] = '\0';
-    
-    return (dest);
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
 #include <stdio.h>
-int main (void)
+int		main(void)
 {
-    char dest[30] = "hola ";
-    char src [] = "caracola";
-    
-    ft_strcat(dest, src);
- 
-    printf("%s\n", dest);
+	char	dest[50] = "caracola";
+	char	*src;
 
-    return 0;
+	src = " hola";
+	printf("%s\n", ft_strcat(dest, src));
+	return 0;
 }
